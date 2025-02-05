a7e7ad469842c8fbb26d91bc654c8f84bad33f88
rinnstu <arpz0601@gmail.com>
2025-02-05 17:06:26 -0300

    Create 16-eje2.c

---
+++ capitulo 2/16-eje2.c
@@ -0,0 +1,17 @@
+/*ejemplo2: Área y circunferencia*/
+/*diseñar un algoritmo que calcule la longitud de la circunferencia y el área de un cı́rculo de radio dado.*/
+
+#include <stdio.h>
+int main () {
+  float radio;
+  float longitud;
+  float area;
+  float PI=3.1416;
+  printf ("ingrese el valor del radio del cı́rculo");
+  scanf ("%f",&radio );
+  longitud=2* PI * radio;
+  area = PI * radio * radio;
+  printf ("La longitud del circulo es: %f", longitud);
+  printf ("El area del circulo es: %f", area);
+  return 0;
+}
